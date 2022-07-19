5a9e9254ac2f9943742a8a09289a2861ee539587
Jacky Kumar Shaw <30664269+JackyKumarShaw@users.noreply.github.com>
2022-07-19 10:14:49 +0530

    ContextAPI

---
+++ src/ComB.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ComC from './ComC'
+export default function ComB() {
+  return (
+    <ComC/>
+  )
+}
---
+++ src/ComC.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FirstName, LastName } from './App'
+export default function ComC() {
+  return (
+    <>
+      <FirstName.Consumer>
+        {(fname) => {
+          return (
+            <LastName.Consumer>
+              {(lname) => {
+                return (
+                  <h1>
+                    My Name is {fname} {lname}
+                  </h1>
+                )
+              }}
+            </LastName.Consumer>
+          )
+        }}
+      </FirstName.Consumer>
+    </>
+  )
+}
