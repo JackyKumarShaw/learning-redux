23e41ce9cfd291abccc05d49b90fe32a91bd031e
Jacky Kumar Shaw <30664269+JackyKumarShaw@users.noreply.github.com>
2022-09-29 02:38:29 +0530

    ContextAPI in its own branch

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+//This is ContextAPI example which is the older technology, the newer technology is the useContextHook which is there in a different branch.
 import { createContext } from 'react'
 import ComA from './ComA'
 const FirstName = createContext()
@@ -5,14 +6,14 @@ const LastName = createContext()
 function App() {
   return (
     <>
-    <FirstName.Provider value={"Jiddu"}>
-      <LastName.Provider value={"Krishnamurti"}>
-        <ComA/>
-      </LastName.Provider>
-    </FirstName.Provider>
+      <FirstName.Provider value={'Jiddu'}>
+        <LastName.Provider value={'Krishnamurti'}>
+          <ComA />
+        </LastName.Provider>
+      </FirstName.Provider>
     </>
-  );
+  )
 }
 
-export default App;
-export { FirstName, LastName };
+export default App
+export { FirstName, LastName }
