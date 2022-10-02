fb11485125e8d9f84c277047dda9c5394e8a84b8
Jacky Kumar Shaw <30664269+JackyKumarShaw@users.noreply.github.com>
2022-10-02 16:36:45 +0530

    Cleaned Main further

--- src/ComB.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ComC from './ComC'
-export default function ComB() {
-  return (
-    <ComC/>
-  )
-}
--- src/ComC.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function ComC() {
-  return <div>ComC</div>
-}
